d5221850db7ad77070c9e5171b5c3d159ac53e54
Ben Chatelain <ben@octop.ad>
2016-02-02 08:13:14 -0700

    Make mirror work regardless of where it is called from:

--- scripts/mirror.rb
+++ scripts/mirror.rb
@@ -6,7 +6,11 @@
 # Updates specs for any new releases of tracked pods.
 #
 
+# File containing the list of pods to mirror. Assumed in same dir as this script.
 pods_list = "pods"
+
+# Switch the current dir to be the one containing this script.
+Dir.chdir(File.dirname(__FILE__))
 
 counter = 1
 begin
